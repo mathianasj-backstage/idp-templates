62f285a7dac73dd9cdaf8cfbc9bd928b191dc396
Joshua Mathianas <mathianasj@gmail.com>
2022-10-27 09:46:25 -0400

    first attempt add cloud event quarkus consumer template

---
+++ scaffolder-templates/quarkus-cloud-event-template/skeleton/src/main/java/io/${{values.java_package_name}}/EventHandler.java
@@ -0,0 +1,15 @@
+package io.${{values.java_package_name}};
+
+import org.jboss.logging.Logger;
+
+import io.quarkus.funqy.Funq;
+import io.smallrye.mutiny.Uni;
+
+public class EventHandler {
+    private static final Logger = Logger.getLogger(EventHandler.class.getName());
+
+    @Funq
+    public Uni<Void> incoming() {
+        return Uni.createFrom().voidItem();
+    }
+}
